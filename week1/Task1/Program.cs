b8dcc7d98a07a82c287c9554e7533270a945d4a0
Islam <islam.fawzy25@hotmail.com>
2022-03-18 13:02:01 +0100

    Task 1 implmented

---
+++ week1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = "world of Csharp";
+string ReverseString(string input){
+    
+char[] inputArray = input.ToCharArray();
+
+Array.Reverse(inputArray);
+return new string (inputArray);
+
+}; //TODO: Implement ReverseString
+string reversed = ReverseString(input);
+Console.WriteLine($"Reversed input value: {reversed}");
+
