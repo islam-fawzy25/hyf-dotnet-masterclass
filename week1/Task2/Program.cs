408ace26fa783138a2026abef6f15daea7c0ec6a
Islam <islam.fawzy25@hotmail.com>
2022-03-19 20:45:47 +0100

    Task2 implmented

---
+++ week1/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Return the number (count) of vowels in the given string. Consider a, e, i, o, u as vowels.
+
+string input = "Intellectualization";
+string vowels = "aeiou" ;
+
+int GetVowelCount(string input){
+int count=0;
+char[] inputArray =input.ToLower().ToCharArray();
+
+foreach(char letter in inputArray){
+ if(vowels.Contains(letter))  count ++ ;
+}
+return count;
+};
+
+int vowelCount = GetVowelCount(input); //TODO: Implement GetVowelCount 
+Console.WriteLine($"Number of vowels: {vowelCount}");
+
+// Example: 
+// 'Intellectualization' => Number of vowels: 9
