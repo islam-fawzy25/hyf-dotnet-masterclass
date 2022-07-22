942c7add52d52c9fc62eb1b0eee4b2b069a734e8
Islam <islam.fawzy25@hotmail.com>
2022-07-22 18:41:18 +0200

    initial

---
+++ week4/test/Program.cs
@@ -0,0 +1,24 @@
+﻿interface I1
+{
+    void Print();
+}
+
+interface I2
+{
+    void Print();
+}
+
+public class Program : I1, I2
+{
+    public void Print(){
+        System.Console.WriteLine("print");
+    }
+    static void Main()
+    {
+        Program P =new Program();
+        P.Print();
+
+
+
+    }
+}
